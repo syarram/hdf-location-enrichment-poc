506d9520d7f927ae694d061561f7256727587d67
yarrams1 <sathesh_kumar@yahoo.com>
2021-05-02 12:51:51 +0100

    Shell scripts - Draft

--- src/main/scripts/hdf_house_keeping.sh
+++ src/main/scripts/hdf_house_keeping.sh
@@ -1,25 +1,27 @@
 #!/bin/bash
 
+if [ $# -ne 2 ]; then
+    echo "************ Need CatalogName and HourstoDelete *****Job failed ${?}"
+    exit 1
+fi
 
-
-location_table=stage_mme_20210415_bkp
 catalog=${1}
-if [
+older_hours=${2}
+
+echo "*************Params passed: ${catalog} and ${older_hours} "
+
+if [ ${catalog} == "MME" ]; then
+table_name=stage_mme_20210415_bkp
+else
+table_name=stage_radius_20210415_bkp
+fi
+
 control_table=webipfr_enrich_control
 log_type=INFO
 delete_batch_size=10000000
-older_hours=${2}
 delete_flag=true
 log_date=`date +%d%m%Y%H%M%S`
-log_dir=/home/yarrams1/hdflogs
-
-
-
-spark-submit --verbose --master yarn-client --driver-memory 15g --executor-memory 10g  --num-executors 4
---executor-cores 5 --jars /home/yarrams1/ipfr-load/jars/shc-core-1.1.1-2.1-s_2.11.jar,/usr/hdp/current/hbase-client/lib/*
---files /usr/hdp/current/hbase-client/conf/hbase-site.xml,/etc/hadoop/conf/core-site.xml
---class com.tef.etl.main.HouseKeeping /home/yarrams1/ipfr-load/etl_batch-0.1.jar stage_mme_20210415_bkp MME webipfr_enrich_control DEBUG 1000000 8 true
-
+log_dir=/home/yarrams1/ipfr-load/logs
 
 
 #submit_saprk_job
@@ -31,15 +33,11 @@ spark-submit \
 --conf spark.sql.shuffle.partitions=200 \
 --jars /home/yarrams1/ipfr-load/jars/shc-core-1.1.1-2.1-s_2.11.jar,/usr/hdp/current/hbase-client/lib/* \
 --files /usr/hdp/current/hbase-client/conf/hbase-site.xml,/etc/hadoop/conf/core-site.xml \
---class class com.tef.etl.main.HouseKeeping \
-/home/yarrams1/ipfr-load/etl_batch-0.1.jar \
-${location_table} \
-${transaction_table} \
-${hdfs_magnet} \
-${hdfs_devicedb} \
-${csp_table} \
-${radius_table} \
-INFO \
-100 \
-${hdfs_enrichment} \
-${control_table} >> $log_dir/spark_job_trigger_$log_date.log 2>&1 &
+--class com.tef.etl.main.HouseKeeping /home/yarrams1/ipfr-load/etl_batch-0.1.jar \
+${table_name} \
+${catalog} \
+${control_table} \
+${log_type} \
+${delete_batch_size} \
+${older_hours} \
+${delete_flag} >> $log_dir/spark_job_hdf_house_keeping_$log_date.log 2>&1 &
